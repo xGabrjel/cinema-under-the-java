853b19d349e60fd3e84af5757c802ee5d302d074
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-11-13 18:28:28 +0100

    Initial commit

---
+++ src/main/java/com/CinemaUnderTheJava/CinemaUnderTheJavaApplication.java
@@ -0,0 +1,13 @@
+package com.CinemaUnderTheJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaUnderTheJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaUnderTheJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CinemaUnderTheJava/CinemaUnderTheJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CinemaUnderTheJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaUnderTheJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
