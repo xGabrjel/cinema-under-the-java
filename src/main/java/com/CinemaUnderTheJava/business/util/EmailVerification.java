e8fedb42b5befdb9c7679d44f8984b23729f93c0
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-11-28 21:20:54 +0100

    Working on sending verification email feature

---
+++ src/main/java/com/CinemaUnderTheJava/business/util/EmailVerification.java
@@ -0,0 +1,35 @@
+package com.cinemaUnderTheJava.business.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Service
+@AllArgsConstructor
+public class EmailVerification {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendVerificationEmail(String sendTo, String url) throws MessagingException, UnsupportedEncodingException {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", Locale.getDefault());
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+        String mailContent = resourceBundle.getString("registration.message1")
+                + "<a href='" + url + "'>" + url + "</a>"
+                + resourceBundle.getString("registration.message2");
+
+        messageHelper.setTo(sendTo);
+        messageHelper.setFrom(resourceBundle.getString("registration.from"), resourceBundle.getString("registration.personal"));
+        messageHelper.setSubject(resourceBundle.getString("registration.subject"));
+        messageHelper.setText(mailContent, true);
+        javaMailSender.send(message);
+    }
+}
