bf3ce4430c8a55d9bd1fbb6e25bcde695b96b2dd
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-11-29 22:38:09 +0100

    Added some work on ticket generation

---
+++ src/main/java/com/CinemaUnderTheJava/business/util/QuickResponseCodeCreator.java
@@ -0,0 +1,42 @@
+package com.cinemaUnderTheJava.business.util;
+
+import com.cinemaUnderTheJava.database.entity.TicketEntity;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Image;
+import lombok.AllArgsConstructor;
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Service
+@AllArgsConstructor
+public class QuickResponseCodeCreator {
+
+    public Element generateQuickResponseCode(String email, TicketEntity ticket) throws BadElementException, IOException {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", Locale.getDefault());
+
+        String content = String.format("%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s",
+                resourceBundle.getString("ticket.name"), ticket.getName(),
+                resourceBundle.getString("ticket.film"), ticket.getFilmTitle(),
+                resourceBundle.getString("ticket.date"), ticket.getProjectionDate(),
+                resourceBundle.getString("ticket.time"), ticket.getProjectionTime(),
+                resourceBundle.getString("ticket.roomNumber"), ticket.getRoomNumber(),
+                resourceBundle.getString("ticket.row"), ticket.getRowNumber(),
+                resourceBundle.getString("ticket.seat"), ticket.getSeatInRow(),
+                resourceBundle.getString("ticket.ticketType"), ticket.getTicketType(),
+                resourceBundle.getString("ticket.ticketPrice"), ticket.getTicketPrice(), ticket.getTicketCurrency(),
+                resourceBundle.getString("ticket.email"), email);
+
+        ByteArrayOutputStream stream = QRCode.from(content)
+                .to(ImageType.PNG)
+                .stream();
+
+        return Image.getInstance(stream.toByteArray());
+    }
+}
