f9cca9249f049101c21b25764622dbd4e036d6ea
xGabrjel <gabriel.luczyszyn@o2.pl>
2024-01-16 13:03:45 +0100

    Test configuration added for further testing

---
+++ src/test/java/com/CinemaUnderTheJava/configuration/support/ControllerTestSupport.java
@@ -0,0 +1,8 @@
+package com.cinemaUnderTheJava.configuration.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public interface ControllerTestSupport {
+
+    ObjectMapper getObjectMapper();
+}
